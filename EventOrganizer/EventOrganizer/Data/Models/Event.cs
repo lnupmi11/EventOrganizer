1ead1e884236dc54c0e7097d8e609ee997212abd
AlexSkorupsky <alex.skorupskyy@gmail.com>
2019-03-17 04:58:11 +0200

    added base models

---
+++ EventOrganizer/EventOrganizer/Data/Models/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventOrganizer.Data.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool IsPreferredEvent { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        public DateTime EventDateTime { get; set; }
+        public DateTime CreationalDateTime { get; set; }
+    }
+}
