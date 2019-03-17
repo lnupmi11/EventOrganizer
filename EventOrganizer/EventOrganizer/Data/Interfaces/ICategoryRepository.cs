c2caec01014b68bbf520c1dcdf80330711ace9c3
AlexSkorupsky <alex.skorupskyy@gmail.com>
2019-03-17 05:11:58 +0200

    added base interfaces

---
+++ EventOrganizer/EventOrganizer/Data/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventOrganizer.Data.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> Categories { get; }
+    }
+}
