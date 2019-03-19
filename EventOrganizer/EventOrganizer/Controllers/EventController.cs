e3e69d0a20b3fbe80834c2b0a30f1d9fea0187c9
AlexSkorupsky <alex.skorupskyy@gmail.com>
2019-03-19 04:03:15 +0200

    added event controller

---
+++ EventOrganizer/EventOrganizer/Controllers/EventController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventOrganizer.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventOrganizer.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IEventRepository _eventRepository;
+
+        public EventController(ICategoryRepository categoryRepository, IEventRepository eventRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _eventRepository = eventRepository;
+        }
+
+        public ViewResult List()
+        {
+            var events = _eventRepository.Events;
+            return View(events);
+        }
+    }
+}
