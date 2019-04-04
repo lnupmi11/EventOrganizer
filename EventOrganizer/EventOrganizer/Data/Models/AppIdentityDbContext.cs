07437fd62e5c5c350aebdc7b9036f52bdafde3f8
Ivan Ternovyi <ivanternovyi@gmail.com>
2019-04-05 01:29:34 +0300

    create the database context class

---
+++ EventOrganizer/EventOrganizer/Data/Models/AppIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventOrganizer.Data.Models
+{
+    class AppIdentityDbContext :  IdentityDbContext<User>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+            : base(options) { }
+    }
+}
