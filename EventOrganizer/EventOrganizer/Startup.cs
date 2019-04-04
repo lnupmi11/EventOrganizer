7239af707db278f2b9c2d085a06b7b5a2b2dd030
Ivan Ternovyi <ivanternovyi@gmail.com>
2019-04-05 01:46:09 +0300

    configure connection string

--- EventOrganizer/EventOrganizer/Startup.cs
+++ EventOrganizer/EventOrganizer/Startup.cs
@@ -8,15 +8,31 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using EventOrganizer.Data.Models;
 
 namespace EventOrganizer
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppIdentityDbContext>(options =>
+                 options.UseSqlServer(
+                 Configuration["Data:EventOrganizerIdentity:ConnectionString"]));
+                            services.AddIdentity<User, IdentityRole>()
+                            .AddEntityFrameworkStores<AppIdentityDbContext>()
+                            .AddDefaultTokenProviders();
+
             services.AddTransient<IEventRepository, MockEventRepository>();
             services.AddTransient<ICategoryRepository, MockCategoryRepository>();
 
@@ -29,6 +45,7 @@ namespace EventOrganizer
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseMvcWithDefaultRoute();
         }
     }
