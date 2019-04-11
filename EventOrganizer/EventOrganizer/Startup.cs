977b638f48abcdf8045ad3550e2dd317fe21f102
AlexSkorupsky <alex.skorupskyy@gmail.com>
2019-04-12 01:49:38 +0300

    ef core configurations

--- EventOrganizer/EventOrganizer/Startup.cs
+++ EventOrganizer/EventOrganizer/Startup.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EventOrganizer.Data;
 using EventOrganizer.Data.Interfaces;
 using EventOrganizer.Data.Mocks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EventOrganizer
@@ -15,8 +18,23 @@ namespace EventOrganizer
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        private IConfigurationRoot _configurationRoot;
+
+        public Startup(IHostingEnvironment hostingEnvironment)
+        {
+            _configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(hostingEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            //Server configuration
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(_configurationRoot.GetConnectionString("DefaultConnection")));
+
             services.AddTransient<IEventRepository, MockEventRepository>();
             services.AddTransient<ICategoryRepository, MockCategoryRepository>();
 
