453a2f7af874834980ad8997dddd9d9b187bd7b5
AlexSkorupsky <alex.skorupskyy@gmail.com>
2019-04-14 20:06:12 +0300

    added home page

---
+++ EventOrganizer/EventOrganizer/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventOrganizer.Data.Interfaces;
+using EventOrganizer.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EventOrganizer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEventRepository _eventRepository;
+
+        public HomeController(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+        public ViewResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PreferredEvents = _eventRepository.PreferredEvents
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ EventOrganizer/EventOrganizer/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using EventOrganizer.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventOrganizer.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Event> PreferredEvents { get; set; }
+    }
+}
