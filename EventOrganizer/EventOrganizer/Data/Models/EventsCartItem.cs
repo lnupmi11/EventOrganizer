d61f5a24d6f294dd58eaf98c1098d4400b8eba40
MaryanTutunnyk <maryan.tutunnyk@gmail.com>
2019-04-18 20:02:03 +0300

    Add events cart item class, migration for cart item; update DbContext script.

---
+++ EventOrganizer/EventOrganizer/Data/Models/EventsCartItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventOrganizer.Data.Models
+{
+    public class EventsCartItem
+    {
+        public int EventsCartItemId { get; set; }
+        public Event Event { get; set; }
+        public string EventsCartId { get; set; }
+    }
+}
--- EventOrganizer/EventOrganizer/Migrations/20190418165502_CartItem.Designer.cs
+++ EventOrganizer/EventOrganizer/Migrations/20190418165502_CartItem.Designer.cs
@@ -4,14 +4,16 @@ using EventOrganizer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EventOrganizer.Migrations
 {
     [DbContext(typeof(EventOrganizerDbContext))]
-    partial class EventOrganizerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190418165502_CartItem")]
+    partial class CartItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
