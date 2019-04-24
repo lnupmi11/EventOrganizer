5da8547558df8291efe2066b591c168c4ce70daf
MaryanTutunnyk <maryan.tutunnyk@gmail.com>
2019-04-25 02:13:45 +0300

    (Add) Add tests for controllers.

---
+++ EventOrganizer/EventOrganizer.Tests/AdminControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using EventOrganizer.Controllers;
+using EventOrganizer.DAL.DbContext;
+using EventOrganizer.DAL.Models;
+using EventOrganizer.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using Xunit;
+
+namespace EventOrganizer.Tests
+{
+    public class AdminControllerTests
+    {
+        //UserManager<User> userManager = new UserManager<User>
+        //    (
+        //    new Mock<IUserStore<User>>().Object,
+        //    new Mock<IOptions<IdentityOptions>>().Object,
+        //    new Mock<IPasswordHasher<User>>().Object,
+        //    new IUserValidator<User>[0],
+        //    new IPasswordValidator<User>[0],
+        //    new Mock<ILookupNormalizer>().Object,
+        //    new Mock<IdentityErrorDescriber>().Object,
+        //    new Mock<IServiceProvider>().Object,
+        //    new Mock<ILogger<UserManager<User>>>().Object
+        //    );
+        //
+        //[Fact]
+        //public void CreateTestNotNull()
+        //{
+        //    Mock<UserManager<User>> userManager = new Mock<UserManager<User>>();
+        //    AdminController adminController = new AdminController(userManager.Object);
+
+        //    CreateModel createModel = new CreateModel();
+        //    var actionResultTask = adminController.Create(createModel);
+        //    actionResultTask.Wait();
+
+        //    var viewResult = actionResultTask.Result as ViewResult;
+        //    Assert.NotNull(viewResult);
+        //}
+    }
+}
---
+++ EventOrganizer/EventOrganizer.Tests/HomeControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using EventOrganizer.BLL.Interfaces;
+using EventOrganizer.Controllers;
+using Moq;
+using Xunit;
+
+namespace EventOrganizer.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexTestNotNull()
+        {
+            Mock<IEventService> eventService = new Mock<IEventService>();
+
+            HomeController homeController = new HomeController(eventService.Object);
+
+            var result = homeController.Index();
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void IndexTestModelNotNull()
+        {
+            Mock<IEventService> eventService = new Mock<IEventService>();
+
+            HomeController homeController = new HomeController(eventService.Object);
+
+            var result = homeController.Index();
+            Assert.NotNull(result.Model);
+        }
+
+        [Fact]
+        public void IndexTestModelNameTrue()
+        {
+            Mock<IEventService> eventService = new Mock<IEventService>();
+
+            HomeController homeController = new HomeController(eventService.Object);
+
+            var result = homeController.Index();
+            Assert.True(string.IsNullOrEmpty(result.ViewName) || result.ViewName == "Index");
+        }
+    }
+}
