17f42900c656738c8d72bdbb058162f06cb059cb
Ivan Ternovyi <ivanternovyi@gmail.com>
2019-05-09 18:33:10 +0300

    implements roles functionality

--- EventOrganizer/EventOrganizer/Controllers/AccountController.cs
+++ EventOrganizer/EventOrganizer/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using EventOrganizer.DAL.Interfaces;
 using EventOrganizer.DAL.Models;
 using EventOrganizer.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
---
+++ EventOrganizer/EventOrganizer/ViewModels/ChangeRoleViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventOrganizer.ViewModels
+{
+    public class ChangeRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string UserEmail { get; set; }
+        public List<IdentityRole> AllRoles { get; set; }
+        public IList<string> UserRoles { get; set; }
+        public ChangeRoleViewModel()
+        {
+            AllRoles = new List<IdentityRole>();
+            UserRoles = new List<string>();
+        }
+    }
+}
