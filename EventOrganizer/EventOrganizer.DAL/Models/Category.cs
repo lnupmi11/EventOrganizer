da4278b1f121ad9f0c1632d38b48152fb0f10b63
Ivan Ternovyi <ivanternovyi@gmail.com>
2019-05-13 21:06:50 +0300

    Rename CategoryId to just Id

--- EventOrganizer/EventOrganizer.DAL/Models/Category.cs
+++ EventOrganizer/EventOrganizer.DAL/Models/Category.cs
@@ -6,7 +6,7 @@ namespace EventOrganizer.DAL.Models
 {
     public class Category
     {
-        public int CategoryId { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public List<Event> Events { get; set; }
