775a2f0c32fecbd593331e7e5273f4e14b9ae872
Andriy Zhuk <31708399+andriy-zhuk@users.noreply.github.com>
2019-05-29 02:01:52 +0300

    added home controller tests
    deleted all previous tests.
    added IndexTest

--- EventOrganizer/EventOrganizer.Tests/HomeControllerTests.cs
+++ EventOrganizer/EventOrganizer.Tests/HomeControllerTests.cs
@@ -1,6 +1,9 @@
 ﻿using EventOrganizer.BLL.Interfaces;
 using EventOrganizer.Controllers;
+using EventOrganizer.DAL.Models;
+using EventOrganizer.ViewModels;
 using Moq;
+using System.Collections.Generic;
 using Xunit;
 
 namespace EventOrganizer.Tests
@@ -8,36 +11,22 @@ namespace EventOrganizer.Tests
     public class HomeControllerTests
     {
         [Fact]
-        public void IndexTestNotNull()
+        public void IndexTest()
         {
             Mock<IEventService> eventService = new Mock<IEventService>();
 
             HomeController homeController = new HomeController(eventService.Object);
-
-            var result = homeController.Index();
-            Assert.NotNull(result);
+            eventService.Setup(item => item.GetPreferredEvents()).Returns
+            (
+                new List<Event>() {TestObjects.Event1, TestObjects.Event2, TestObjects.Event3 }
+            );
+            var resultModel = homeController.Index().Model;
+            var expected = new HomeViewModel
+            {
+                PreferredEvents = new List<Event>() { TestObjects.Event1, TestObjects.Event2, TestObjects.Event3 }
+            };
+            Assert.Equal(resultModel.ToString(), expected.ToString());
         }
-
-        [Fact]
-        public void IndexTestModelNotNull()
-        {
-            Mock<IEventService> eventService = new Mock<IEventService>();
-
-            HomeController homeController = new HomeController(eventService.Object);
-
-            var result = homeController.Index();
-            Assert.NotNull(result.Model);
-        }
-
-        [Fact]
-        public void IndexTestModelNameTrue()
-        {
-            Mock<IEventService> eventService = new Mock<IEventService>();
-
-            HomeController homeController = new HomeController(eventService.Object);
-
-            var result = homeController.Index();
-            Assert.True(string.IsNullOrEmpty(result.ViewName) || result.ViewName == "Index");
-        }
+        
     }
 }
