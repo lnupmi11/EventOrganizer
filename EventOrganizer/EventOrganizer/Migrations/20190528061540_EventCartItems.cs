9496bb104512cf011417da45b6b55da38dc2c3e3
MaryanTutunnyk <maryan.tutunnyk@gmail.com>
2019-05-28 09:18:42 +0300

    (update) Update database structure.

---
+++ EventOrganizer/EventOrganizer/Migrations/20190528061540_EventCartItems.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventOrganizer.Migrations
+{
+    public partial class EventCartItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EventCartItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EventId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventCartItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EventCartItems_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventCartItems_EventId",
+                table: "EventCartItems",
+                column: "EventId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventCartItems");
+        }
+    }
+}
