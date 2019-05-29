1f9731562905b1e1a3c0d597f65a16e585c522d7
Andriy Zhuk <31708399+andriy-zhuk@users.noreply.github.com>
2019-05-29 13:52:48 +0300

    added userpage  controller tests
    added index not null test

---
+++ EventOrganizer/EventOrganizer.Tests/UserPageControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using EventOrganizer.Controllers;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace EventOrganizer.Tests
+{
+    public class UserPageControllerTests
+    {
+        [Fact]
+        public void IndexTestNotNull()
+        {
+            UserPageController contactController = new UserPageController();
+
+            var result = contactController.Index();
+            Assert.NotNull(result);
+        }
+    }
+}
