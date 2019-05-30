7d91903243aba195ae5af5fb30973b4e4a34421a
Andriy Zhuk <31708399+andriy-zhuk@users.noreply.github.com>
2019-05-30 20:07:07 +0300

    fixed tests bugs and style updates
    fixed tests bugs ommited after change in userPageController
    Style update: official can edit and delete only his "own" events

--- EventOrganizer/EventOrganizer.Tests/UserPageControllerTests.cs
+++ EventOrganizer/EventOrganizer.Tests/UserPageControllerTests.cs
@@ -1,7 +1,11 @@
-﻿using EventOrganizer.Controllers;
+﻿using EventOrganizer.BLL.Interfaces;
+using EventOrganizer.Controllers;
+using EventOrganizer.DAL.Models;
+using Microsoft.AspNetCore.Identity;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text;
 using Xunit;
 
@@ -9,12 +13,38 @@ namespace EventOrganizer.Tests
 {
     public class UserPageControllerTests
     {
+        private static readonly IEnumerable<User> TestUsers = new[]
+        {
+            TestObjects.User1,
+            TestObjects.User2,
+            TestObjects.User3
+        };
+        private static readonly IEnumerable<Event> TestEvents = new[]
+        {
+            TestObjects.Event1,
+            TestObjects.Event2,
+            TestObjects.Event3
+        };
+        private Mock<UserManager<User>> GetUserManagerMock()
+        {
+            var userStore = new Mock<IUserStore<User>>();
+            var userManager = new Mock<UserManager<User>>(userStore.Object, null, null, null, null, null, null, null, null);
+            return userManager;
+        }
         [Fact]
         public void IndexTestNotNull()
         {
-            UserPageController contactController = new UserPageController();
+            var eventService = new Mock<IEventService>();
+            var userManager = GetUserManagerMock();
+            UserPageController userPageController = new UserPageController(eventService.Object, userManager.Object);
+            userManager.Setup(item => item.GetUserId(It.IsAny<ClaimsPrincipal>())).Returns("1");
+            eventService.Setup(item => item.GetEventsByUserId(It.IsAny<string>())).Returns(TestEvents);
 
-            var result = contactController.Index();
+            var result = userPageController.Index();
+            var actual = userPageController.ViewData["UserEvents"].ToString();
+            var expected = TestEvents.ToString();
+
+            Assert.Equal(expected, actual);
             Assert.NotNull(result);
         }
     }
